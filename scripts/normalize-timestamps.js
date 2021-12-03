8f67a027f35fd936f672dbafd5754635148dab2a
Doug Martin <dmartin@concord.org>
2021-12-03 08:19:15 -0500

    Finished normalize-timestamps script

--- scripts/normalize-timestamps.js
+++ scripts/normalize-timestamps.js
@@ -1,46 +1,68 @@
 const {localConnect} = require("./shared/db")
-const Cursor = require("pg-cursor")
+const format = require('pg-format');
 
-const BATCH_SIZE = 100
 const WINDOW_SIZE = 100
 
+/*
+1. find all times outside of range
+2. for each time find the nearest time in range
+3. average the two times
+*/
+
 localConnect().then(localClient => {
-  const query = `
-    SELECT
-      remote_id, time,
-      AVG(extract(epoch from time))
-    OVER (PARTITION BY remote_id ORDER BY remote_id ROWS BETWEEN ${WINDOW_SIZE} PRECEDING AND ${WINDOW_SIZE} FOLLOWING) AS time_average
-    FROM logs_meta
-    WHERE time < '2014-01-01T00:00:00.000Z' OR time > now()::timestamp
-  `
-  const cursor = localClient.query(new Cursor(query))
+  const getOutOfRangeRows = () => {
+    const query = `
+      SELECT remote_id
+      FROM logs_meta
+      WHERE time < '2014-01-01T00:00:00.000Z' OR time > now()::timestamp
+    `
+    return localClient.query(query)
+      .then(result => {
+        const fixedTimeMap = {}
+        console.log(`Finding nearest time for ${result.rows.length} rows with bad times`)
+        const promises = result.rows.map(row => {
+          const closestIdsQuery = format(`
+            SELECT remote_id, extract(epoch from time) as unix_time, time FROM
+            (
+              (SELECT * FROM logs_meta WHERE remote_id >= %L AND time >= '2014-01-01T00:00:00.000Z' AND time <= now()::timestamp ORDER BY remote_id LIMIT 1)
+              UNION ALL
+              (SELECT * FROM logs_meta WHERE remote_id < %L AND time >= '2014-01-01T00:00:00.000Z' AND time <= now()::timestamp ORDER BY remote_id DESC LIMIT 1)
+            ) AS foo
+          `, row.remote_id, row.remote_id)
 
-  const rowsToUpdate = []
+          return localClient.query(closestIdsQuery)
+            .then(result => {
+              if (result.rows.length !== 2) {
+                console.error(`ONLY ${result.rows.length} ROW FOUND!`)
+                process.exit()
+              }
+              fixedTimeMap[row.remote_id] = (result.rows[0].unix_time + result.rows[1].unix_time ) / 2
+            })
+        })
 
-  const processResults = () => {
-    return new Promise((resolve, reject) => {
-      (function read() {
-        cursor.read(BATCH_SIZE, async (err, rows) => {
-          if (err) {
-            return reject(err);
-          }
-
-          if (!rows.length) {
-            return resolve();
-          }
-
-          rowsToUpdate = rowsToUpdate.concat(rows)
-
-          return read();
-        });
-      })();
-    });
+        return Promise.all(promises)
+          .then(() => fixedTimeMap)
+      })
   }
 
-  processResults()
-    .then(() => {
-      console.log("ROWS TO UPDATE", rowsToUpdate)
+  const updateOutOfRangeRows = (fixedTimeMap) => {
+    const ids = Object.keys(fixedTimeMap)
+    console.log(`Found ${ids.length} rows to update`)
+    const promises = ids.map(id => {
+      const query = format(`
+        UPDATE logs_meta
+        SET
+          time = to_timestamp(%L) at time zone 'utc',
+          status = status | 1
+        WHERE remote_id = %L
+      `, fixedTimeMap[id], id)
+      return localClient.query(query)
     })
+    return Promise.all(promises)
+  }
+
+  getOutOfRangeRows()
+    .then(updateOutOfRangeRows)
     .catch(err => console.error(err))
     .finally(() => process.exit())
 })
