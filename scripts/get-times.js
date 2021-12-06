1092fbee9ae22c13a7c7a0e6995cb3bec56d284c
Doug Martin <dmartin@concord.org>
2021-12-06 14:57:09 -0500

    Updated get-times to show timestamps

--- scripts/get-times.js
+++ scripts/get-times.js
@@ -2,7 +2,7 @@ const {localConnect, remoteConnect} = require("./shared/db")
 
 localConnect().then(localClient => {
   remoteConnect().then(remoteClient => {
-    return localClient.query("SELECT MIN(time) AS min_time, MAX(time) AS max_time FROM logs_meta")
+    return localClient.query("SELECT MIN(time) AS min_time, MAX(time) AS max_time, MIN(timestamp) AS min_timestamp, MAX(timestamp) AS max_timestamp FROM logs_meta")
       .then(result => console.log("LOCAL", result.rows))
       .then(() => {
         return remoteClient.query("SELECT MIN(time) AS min_time, MAX(time) AS max_time FROM logs")
